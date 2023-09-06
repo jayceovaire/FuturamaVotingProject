9c65d697c9b2baae3802169ae63ab99a89cbcc46
jayceovaire <118041687+jayceovaire@users.noreply.github.com>
2023-09-06 19:51:53 -0400

    added Episode voting page

---
+++ routes/EpisodeVotingPage.tsx
@@ -0,0 +1,14 @@
+import Picker from "../islands/Picker.tsx";
+import Navbar from "../islands/Nav.tsx";
+
+
+export default function EpisodeVotingPage(){
+
+
+    return(
+        <>
+            <Navbar/>
+        <Picker />
+        </>
+        )
+}
