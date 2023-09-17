7294a4ca43979fc4af6f4bb83df31ed24c77208f
jayceovaire <118041687+jayceovaire@users.noreply.github.com>
2023-09-16 20:41:18 -0400

    added season directories for episodes, finished season 3, fixed infocard image sizing

--- islands/InfoCard.tsx
+++ islands/InfoCard.tsx
@@ -10,7 +10,7 @@ export default function InfoCard(props: InfoCardProps) {
 
     return (
         <div className={'border border-4 border-black sticky top-0 rounded-md'}>
-            <img src={imageState} alt={""} className={''} />
+            <img src={imageState} alt={""} className={'w-[50rem] h-[15rem]'} />
             <div>Episode #{hoverState}</div>
             <p>Card Description</p>
         </div>
